ffb5ce59e75f3894db8fea07da59d9adbb3e0b82
JasonLGreen2001 <jasonlgreen2001@gmail.com>
2021-09-06 10:58:50 -0700

    First Commit

---
+++ build/CMakeFiles/Lab2-6.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab2-6.dir/source/main.cpp.obj"
+  "Lab2-6.exe"
+  "Lab2-6.exe.manifest"
+  "Lab2-6.pdb"
+  "libLab2-6.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab2-6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
